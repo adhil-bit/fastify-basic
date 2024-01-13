324eecba9bb4dac9d4084102f4901756d2febd3e
adhil-dataspot <adhilashraf76@gmail.com>
2024-01-14 00:07:53 +0530

    fastify basic test

---
+++ index.js
@@ -0,0 +1,15 @@
+
+const fastify = require('fastify')({
+  logger: true
+})
+
+// Declare a route
+fastify.get('/', (request, reply) => {
+  reply.send({ hello: 'world' })
+})
+
+// Run the server!
+fastify.listen({ port: 3001 }, (err, address) => {
+  if (err) throw err
+  // Server is now listening on ${address}
+})
